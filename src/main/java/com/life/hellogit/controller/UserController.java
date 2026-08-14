22673c8faf46654eb159c6742691bba00fed9ea9
HP <2890085490@qq.com>
2026-08-14 23:36:37 +0800

    修改了hello语句

--- src/main/java/com/life/hellogit/controller/UserController.java
+++ src/main/java/com/life/hellogit/controller/UserController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Git!";
+        return "Hello, Gity!";
     }
 }
